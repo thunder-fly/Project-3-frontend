318d542b166dfd923f27747efcc55575ba46390e
jingrid <ijin@bu.edu>
2017-08-18 14:08:54 -0400

    fix jQuery ID undefined error
    fix jQuery error that pops up when trying to view all pages and all
    blogs when user is not signed in.

--- assets/scripts/templates/helpers/checkUser.js
+++ assets/scripts/templates/helpers/checkUser.js
@@ -1,8 +1,15 @@
 const store = require('../../store')
 
 module.exports = function (userId) {
-  if (userId === store.user.id) {
+  if (store.user && (store.user.id === userId)) {
     return '<button type="submit" class="remove-button" name="remove-button">Remove</button>  <button type="submit" class="edit-button" name="edit-button">Edit</button>'
   } else {
   }
 }
+
+// module.exports = function (userId) {
+//   if (userId === store.user.id) {
+//     return '<button type="submit" class="remove-button" name="remove-button">Remove</button>  <button type="submit" class="edit-button" name="edit-button">Edit</button>'
+//   } else {
+//   }
+// }
