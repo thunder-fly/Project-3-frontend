bc2a029c2f1d94337f4caaa828ffeef06f9cf6eb
jingrid <ijin@bu.edu>
2017-10-12 17:49:05 -0400

    add styling to auth elements and background img

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,9 @@
 'use strict'
 
-const host = 'https://guarded-bastion-33741.herokuapp.com'
+const host = 'http://localhost:4741'
 
 module.exports = {
   host
 }
+
+// https://guarded-bastion-33741.herokuapp.com
